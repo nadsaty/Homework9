4aba64a92c18abec668963c82b27223f3d45aaf9
nadsay <nadsatyyy@gmail.com>
2023-08-27 15:03:19 +0300

    Домашка 9, Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -15,8 +16,30 @@ public class Main {
     public static void task1() {
         int[] bookKeeping = generateRandomArray();
         System.out.println("\n Задача 1 \n");
+
         for (int i : bookKeeping) {
             System.out.println("Сумма трат за месяц составила " + i + " рублей");
         }
+    }
+
+    public static void task2() {
+        int[] bookKeeping = generateRandomArray();
+        int max = -1;
+        int min = 200001;
+        System.out.println("\n Задача 2 \n");
+
+        for (int i = 0; i < bookKeeping.length; i++) {
+            if (bookKeeping[i] > max) {
+                max = bookKeeping[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+
+        for (int i = 0; i < bookKeeping.length; i++) {
+            if (bookKeeping[i] < min) {
+                min = bookKeeping[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
     }
 }
